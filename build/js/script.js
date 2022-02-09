d75bad9ee5cf2aaace22f7036ad7c9a89e6932e0
Kotwade <kotwade@yandex.ru>
2022-02-09 11:22:18 +0500

    Исправления 4

--- build/js/script.js
+++ build/js/script.js
@@ -1,30 +1,6 @@
 'use strict';
 var ESC_KEY = 'Escape';
 
-const swiper = new Swiper('.swiper-container', {
-  // Default parameters
-  slidesPerView: 1,
-  spaceBetween: 10,
-  // Responsive breakpoints
-  breakpoints: {
-    // when window width is >= 320px
-    320: {
-      slidesPerView: 1,
-      slidesPerGroup: 1,
-      spaceBetween: 10
-    },
-    1024: {
-      slidesPerView: 1,
-      slidesPerGroup: 1,
-      spaceBetween: 10
-    }
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev'
-  }
-});
-
 // Попапы
 
 var sedona = document.querySelector('.photo-sedona-popup');
